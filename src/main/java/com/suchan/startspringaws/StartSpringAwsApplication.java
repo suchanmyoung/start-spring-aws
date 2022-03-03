22a7dab78e7f5d8e55afc18b24766bfe88deb093
suchanmyung <aud248@naver.com>
2022-03-03 22:59:05 +0900

    Initial commit

---
+++ src/main/java/com/suchan/startspringaws/StartSpringAwsApplication.java
@@ -0,0 +1,13 @@
+package com.suchan.startspringaws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartSpringAwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StartSpringAwsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/suchan/startspringaws/StartSpringAwsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suchan.startspringaws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartSpringAwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
