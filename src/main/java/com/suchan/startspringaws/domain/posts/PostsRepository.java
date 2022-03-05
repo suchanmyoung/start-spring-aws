e23300f451378d05ba11b99bc6d185d7650655b9
suchanmyung <aud248@naver.com>
2022-03-05 22:19:02 +0900

    feat: Mustache Server-side-rendering 2

--- src/main/java/com/suchan/startspringaws/domain/posts/PostsRepository.java
+++ src/main/java/com/suchan/startspringaws/domain/posts/PostsRepository.java
@@ -1,4 +1,8 @@
 package com.suchan.startspringaws.domain.posts;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 /*
     보통 매퍼에서 Dao라고 불리는 DB Layer 접근자
@@ -6,7 +10,10 @@ package com.suchan.startspringaws.domain.posts;
     JpaRepository를 상속 받으면 CRUD 메소드가 자동으로 생성
  */
 
-import org.springframework.data.jpa.repository.JpaRepository;
+
 
 public interface PostsRepository extends JpaRepository<Posts, Long> {
+
+    @Query("SELECT p FROM Posts p ORDER BY p.id DESC")
+    List<Posts> findAllDesc();
 }
--- src/main/java/com/suchan/startspringaws/service/PostsService.java
+++ src/main/java/com/suchan/startspringaws/service/PostsService.java
@@ -2,12 +2,16 @@ package com.suchan.startspringaws.service;
 
 import com.suchan.startspringaws.domain.posts.Posts;
 import com.suchan.startspringaws.domain.posts.PostsRepository;
+import com.suchan.startspringaws.web.dto.PostsListResponseDto;
 import com.suchan.startspringaws.web.dto.PostsResponseDto;
 import com.suchan.startspringaws.web.dto.PostsSaveRequestDto;
 import com.suchan.startspringaws.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 /*
     @RequiredArgsConstructor가 final이 선언된 모든 필드를 인자값으로 하는 생성자를 생성해준다.
@@ -40,9 +44,22 @@ public class PostsService {
         return id;
     }
 
-    public PostsResponseDto findById(Long id){
+    public PostsResponseDto findById(Long id) {
         Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id = " + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream().map(PostsListResponseDto::new).collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete(Long id) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id = " + id));
+
+        postsRepository.delete(posts);
     }
 }
