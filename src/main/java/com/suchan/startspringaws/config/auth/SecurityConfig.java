5503be16bb05f1de4d670590a2ae1929a75d14a7
suchanmyung <aud248@naver.com>
2022-03-05 23:41:25 +0900

    feat: Google Login 1

---
+++ src/main/java/com/suchan/startspringaws/config/auth/SecurityConfig.java
@@ -0,0 +1,39 @@
+package com.suchan.startspringaws.config.auth;
+
+import com.suchan.startspringaws.domain.user.Role;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/*
+    @EnableWebSecurity : Spring Security 설정 활성화
+    authorizeRequests : URL 별 권한 관리를 설정하는 옵션의 시작점
+    antMatchers : 권한 관리 대상을 지정하는 옵션
+    anyMatchers : 설정된 값들 이외 나머지 URL. 여기서는 authenticated를 추가하여 나머지 URL들은 모두 인증된 사용자들에게만 허용도록 함
+ */
+
+@RequiredArgsConstructor
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    private final CustomOAuth2UserService customOAuth2UserService;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception{
+        http
+                .csrf().disable()
+                .headers().frameOptions().disable()
+                .and()
+                .authorizeRequests()
+                .antMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**").permitAll()
+                .antMatchers("/api/v1/**").hasRole(Role.USER.name())
+                .anyRequest().authenticated()
+                .and()
+                .logout()
+                .logoutSuccessUrl("/")
+                .and()
+                .oauth2Login()
+                .userInfoEndpoint()
+                .userService(customOAuth2UserService);
+    }
+}
---
+++ src/main/java/com/suchan/startspringaws/domain/user/Role.java
@@ -0,0 +1,19 @@
+package com.suchan.startspringaws.domain.user;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+/*
+    Security에서는 권한 코드에 항상 ROLE_가 붙어있어야 한다.
+ */
+
+@Getter
+@RequiredArgsConstructor
+public enum Role {
+
+    GUEST("ROLE_GUEST", "손님"),
+    USER("ROLE_USER", "일반 사용자");
+
+    private final String key;
+    private final String title;
+}
